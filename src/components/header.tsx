9e02eecefc54b8d2232a4b981de31307bfd00b85
Mohit Dogra <158460926+Mohitxdogra@users.noreply.github.com>
2025-02-03 13:48:27 +0530

    changes: Header components completed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,30 +2,55 @@ import { cn } from "@/lib/utils";
 import { useAuth } from "@clerk/clerk-react";
 import { Container } from "./container";
 import { LogoContainer } from "./logo-container";
+import { NavigationRoutes } from "./Navigation-routes";
+import { NavLink } from "react-router-dom";
+import { ProfileContainer } from "./profile-container";
+import { ToggleContainer } from "./toggle-container";
 
-const Header =( ) => {
-const{userId}=useAuth();
+
+
+const Header = () => {
+  const { userId } = useAuth();
+
   return (
-  <header 
-  className={cn("w-full border-b duration-150 transition-all ease-in-out")}
-    >
-    
-    <Container>
-      <div className="flex items-center gap-4 w-full">
-        {/*logo section*/}
-<LogoContainer/>
-        {/*nav section*/}
-<nav className="hidden md:flex items-center gap-3">
-      <ul className="flex items-center gap-6">
-        {}
-        </ul>
-        </nav>
-        {/*profile section*/}
-      </div>
-    </Container>
-    
-    
+    <header className={cn("w-full border-b duration-150 transition-all ease-in-out")}>
+      <Container>
+        <div className="flex items-center gap-4 w-full">
+          {/* Logo section */}
+          <LogoContainer />
+
+          {/* Nav section */}
+          <nav className="hidden md:flex items-center gap-3">
+            <NavigationRoutes />
+            {userId && (
+              <NavLink
+                to={"/generate"}
+                className={({ isActive }) =>
+                  cn("text-base text-neutral-600", 
+                    isActive && "text-neutral-900 font-semibold")
+                }
+              >
+                Take an Interview
+              </NavLink>
+            )}
+          </nav>
+
+         
+          <div className="ml-auto flex items-center gap-6">
+        {/*profile section */}
+       <ProfileContainer/>
+
+
+
+
+
+        {/*mobile toggle section */}
+      <ToggleContainer/>
+        </div> 
+        </div>
+      </Container>
     </header>
   );
 };
+
 export default Header;
---
+++ src/lib/helpers.tsx
@@ -0,0 +1,20 @@
+export const MainRoutes=[
+    {
+        label:"Home",
+    href:"/",
+    },
+
+    {
+        label:"Contact Us",
+        href:"/Contact",
+        },
+
+        {
+            label:"About Us",
+            href:"/about",
+            },
+            {
+                label:"Services",
+                href:"/services",
+                },
+];
