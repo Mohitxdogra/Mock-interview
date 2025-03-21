3ccea17c6a97a6bd689d78f201c5e0a7d48d83da
Mohit Dogra <158460926+Mohitxdogra@users.noreply.github.com>
2025-03-21 23:42:20 +0530

    completed

--- src/routes/create-edit-page.tsx
+++ src/routes/create-edit-page.tsx
@@ -1,34 +1,37 @@
-import { db } from "@/config/firebase.config";
-
 import { FormMockInterview } from "@/components/form-mock-interview";
+import { db } from "@/config/firebase.config";
 import { Interview } from "@/types";
 import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export const CreateEditPage = () => {
-    const { interviewId } = useParams<{ interviewId: string }>();
-    const [interview, setInterview] = useState<Interview | null>(null);
+  const { interviewId } = useParams<{ interviewId: string }>();
+  const [interview, setInterview] = useState<Interview | null>(null);
 
-    useEffect(() => {
-        const fetchInterview = async () => {
-            if (interviewId) {
-                try {
-                    const interviewDoc = await getDoc(doc(db, "interviews", interviewId));
-                    if (interviewDoc.exists()) {
-                        setInterview({ ...interviewDoc.data() } as Interview);
-                    }
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-        };
-        fetchInterview();
-    }, [interviewId]);
+  useEffect(() => {
+    const fetchInterview = async () => {
+      if (interviewId) {
+        try {
+          const interviewDoc = await getDoc(doc(db, "interviews", interviewId));
+          if (interviewDoc.exists()) {
+            setInterview({
+              id: interviewDoc.id,
+              ...interviewDoc.data(),
+            } as Interview);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
 
-    return (
-        <div className="my-4 flex-col w-full">
-            <FormMockInterview initialData={interview} />
-        </div>
-    );
+    fetchInterview();
+  }, [interviewId]);
+
+  return (
+    <div className="my-4 flex-col w-full">
+      <FormMockInterview initialData={interview} />
+    </div>
+  );
 };
